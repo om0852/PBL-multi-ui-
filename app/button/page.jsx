3da19e49a57486f0ee7833c136ac7882df010a86
om salunke <omsaunke343@gmail.com>
2025-01-24 11:06:28 +0530

    Button 4 to 10 added

---
+++ app/button/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react'
+import Button4 from './_components/Button_4'
+
+const page = () => {
+  return (
+    <div>
+        <Button4 text="Button 1" />
+      
+    </div>
+  )
+}
+
+export default page
